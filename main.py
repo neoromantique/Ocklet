3272bf00c01e75c5bf6666ce058e5d59c9385b59
David Aizenberg <david@aizenberg.co.uk>
2020-04-17 18:10:39 +0300

    add api

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 from contextlib import closing
 
-from os import listdir, mkdir, symlink, _exit, environ
+from os import listdir, mkdir, symlink, _exit, environ, walk, path
 from os.path import isdir, join, dirname, exists, splitext
 from PIL import Image
 
-
+import os.path
 import subprocess
+import json
 from pathlib import Path
 
 debug = False
@@ -30,9 +31,12 @@ app.config.update(dict(
     THUMB_SIZE=(256, 256)
 ))
 
+
 @app.route('/restart', defaults={'path': ''})
 def restart(path):
     _exit(11)
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def photo_dir(path):
@@ -55,7 +59,7 @@ def photo_dir(path):
             if isdir(join(nav, list_item)) and list_item != '.thumbnail':
                 dir_entries.append(list_item)
             elif list_item.endswith('.png') or list_item.endswith('.jpg') or list_item.endswith('.JPG') or list_item.endswith('.jpeg'):
-                # Generate missing thumbnails 
+                # Generate missing thumbnails
                 thumb = join(thumb_dir, list_item)
                 thumbjpg = Path(thumb).stem + '.jpg'
                 if not exists(str(Path(thumb).parent) + '/' + thumbjpg):
@@ -68,15 +72,39 @@ def photo_dir(path):
                 image_entries.append([list_item, thumbjpg])
 
             elif list_item.endswith('.mp4') or list_item.endswith('.webp'):
-                # Generate missing thumbnails 
+                # Generate missing thumbnails
                 thumb = join(thumb_dir, list_item)
                 thumbjpg = Path(thumb).stem + '.jpg'
                 if not exists(str(Path(thumb).parent) + '/' + thumbjpg):
                     subprocess.call(['ffmpeg', '-i', join(nav, list_item), '-ss', '00:00:00.000',
-                                      '-vframes', '1', str(Path(thumb).parent) + '/' + thumbjpg])
+                                     '-vframes', '1', str(Path(thumb).parent) + '/' + thumbjpg])
                 image_entries.append([list_item, thumbjpg])
 
         return render_template('layout.html', dir_entries=dir_entries, image_entries=image_entries, path=path, up=up, proto=proto, base=app.config['FQDN'])
+
+
+@app.route('/api/', defaults={'path': ''})
+@app.route('/api/<path:path>')
+def photo_feed(path):
+    base = app.config['PHOTO_DIR']
+    up = None
+    if path != '':
+        up = dirname(join('/', path))
+
+    entries = {"domain": proto + '://' + app.config['FQDN'], "up": up, "dir": [], "img": []}
+
+    dir = join(base + '/' + path)
+
+    for list_item in sorted(listdir(dir)):
+        if isdir(join(dir, list_item)) and list_item != '.thumbnail':
+            entries['dir'].append(list_item)
+        elif list_item.endswith('.png') or list_item.endswith('.jpg') or list_item.endswith('.JPG') or list_item.endswith('.jpeg'):
+            entries['img'].append(list_item)
+        elif list_item.endswith('.mp4') or list_item.endswith('.webp') or list_item.endswith('.gif'):
+            entries['img'].append(list_item)
+
+    return(json.dumps(entries))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
